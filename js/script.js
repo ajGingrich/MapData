cd8ea0f9afb07c46b896fa8f0bac64ff3c7dcd90
Andrew Gingrich <gandrew320@gmail.com>
2017-10-20 07:59:08 -0600

    fix margin

--- js/script.js
+++ js/script.js
@@ -13,7 +13,7 @@ $.ajax({
             success: function(locations) {
 
                 ///set margins
-                var margin = {top: 10, right: 10, bottom: 10, left:10};
+                var margin = {top: 0, right: 0, bottom: 0, left:0};
                 var height = 550 - margin.top - margin.bottom;
                 var width = 1000 - margin.right - margin.left;
 
